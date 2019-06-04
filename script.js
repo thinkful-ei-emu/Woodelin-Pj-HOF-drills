9cc3593f1d45596373518bbdbe25b52951d93535
Prabhjot Singh <pjsingh1359@outlook.com>
2019-06-04 14:34:57 -0700

    finished filter drill

--- script.js
+++ script.js
@@ -13,5 +13,33 @@ function goodbye() {
   console.log('Goodbye world');
 }
 
-repeat(hello, 5);
-repeat(goodbye, 2);
+// repeat(hello, 5);
+// repeat(goodbye, 2);
+
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+function filter(arr,fn) {
+  let newArray = [];
+  for(let i = 0; i < arr.length; i++) {
+    if(fn(arr[i])){
+      newArray.push(arr[i]);
+    }
+  }
+  // TASK: Define your function here
+  return newArray;
+}
+
+// DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
+// First we setup an array of strings we plan to filter:
+
+
+// We use your `filter` function here, capturing a new array into `filteredNames`
+// comprised of names that only begin with 'R'
+const filteredNames = filter(myNames, function(name) {
+  // This is known as a "predicate function" - it's a function that 
+  // only returns a boolean
+  return name[0] === 'R';
+});
+
+console.log(filteredNames) // => ['Rich', 'Ray']
+// <---- DO NOT EDIT BETWEEN THESE LINES
